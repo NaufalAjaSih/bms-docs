d7acf802d4d2115acc0f3cbe549861ceee55d13a
NaufalAjaSih <108167231+NaufalAjaSih@users.noreply.github.com>
2025-08-06 11:23:30 +0700

    Standar CRUD Laravel 12 Docs

---
+++ src/app/_meta.js
@@ -0,0 +1,9 @@
+export default {
+  "CRUD Laravel 12": {
+    title: "CRUD Laravel 12",
+    type: "separator",
+  },
+  structure: "Struktur Project",
+  controller: "Controller Standart",
+  model: "Model Standart",
+};
